49ffe9c44e0b047377a3875857193309e14e11f2
retarfi <valerososoccer@gmail.com>
2023-05-21 00:02:21 +0900

    fix: load MPT properly and move load_lora_model

--- fastchat-cli.py
+++ fastchat-cli.py
@@ -1,26 +1,23 @@
 """
 Chat with a model with command line interface.
-
-Usage:
-python3 -m fastchat.serve.cli --model ~/model_weights/llama-7b
 """
 import argparse
 import contextlib
-import os
-from typing import Callable, Optional, Tuple, Union
+from typing import Callable
 from unittest.mock import patch
 
-import torch
 from fastchat.conversation import register_conv_template, Conversation, SeparatorStyle
 from fastchat.serve import cli
-from fastchat.model.model_adapter import add_model_args, load_model, MPTAdapter
-from peft import LoraConfig, set_peft_model_state_dict, get_peft_model, PeftModel
-from transformers import PreTrainedModel, PreTrainedTokenizerBase
+from fastchat.model.model_adapter import add_model_args, MPTAdapter, model_adapters
 from unittest import mock
 
 from jallm.models.model_adapter import PatchedMPTAdapter
+from jallm.modeling_utils import load_lora_model
 
-MPTAdapter = PatchedMPTAdapter
+
+for i, adapter in enumerate(model_adapters):
+    if adapter.__class__.__name__ == "MPTAdapter":
+        model_adapters[i] = PatchedMPTAdapter()
 
 
 register_conv_template(
@@ -40,61 +37,15 @@ register_conv_template(
 Conversation._get_prompt = Conversation.get_prompt
 Conversation._append_message = Conversation.append_message
 
+
 def conversation_append_message(cls, role: str, message: str):
     cls.offset = -2
     return cls._append_message(role, message)
 
+
 def conversation_get_prompt_overrider(cls: Conversation) -> str:
     cls.messages = cls.messages[-2:]
     return cls._get_prompt()
-
-
-def load_lora_model(
-    model_path: str,
-    lora_weight: str,
-    device: str,
-    num_gpus: int,
-    max_gpu_memory: Optional[str] = None,
-    load_8bit: bool = False,
-    cpu_offloading: bool = False,
-    debug: bool = False,
-) -> Tuple[Union[PreTrainedModel, PeftModel], PreTrainedTokenizerBase]:
-    model: Union[PreTrainedModel, PeftModel]
-    tokenizer: PreTrainedTokenizerBase
-    model, tokenizer = load_model(
-        model_path=model_path,
-        device=device,
-        num_gpus=num_gpus,
-        max_gpu_memory=max_gpu_memory,
-        load_8bit=load_8bit,
-        cpu_offloading=cpu_offloading,
-        debug=debug,
-    )
-    if lora_weight is not None:
-        # model = PeftModelForCausalLM.from_pretrained(model, model_path, **kwargs)
-        config = LoraConfig.from_pretrained(lora_weight)
-        model = get_peft_model(model, config)
-
-        # Check the available weights and load them
-        checkpoint_name = os.path.join(
-            lora_weight, "pytorch_model.bin"
-        )  # Full checkpoint
-        if not os.path.exists(checkpoint_name):
-            checkpoint_name = os.path.join(
-                lora_weight, "adapter_model.bin"
-            )  # only LoRA model - LoRA config above has to fit
-        # The two files above have a different name depending on how they were saved,
-        # but are actually the same.
-        if os.path.exists(checkpoint_name):
-            adapters_weights = torch.load(checkpoint_name)
-            set_peft_model_state_dict(model, adapters_weights)
-        else:
-            print(f"Checkpoint {checkpoint_name} not found")
-
-    if debug:
-        print(model)
-
-    return model, tokenizer
 
 
 @patch("fastchat.serve.inference.load_model", autospec=True)
@@ -128,9 +79,13 @@ def main(
     )
     if args.conv_template is not None:
         print(f"conv_template: {args.conv_template}")
-        
-    with mock.patch.object(Conversation, "get_prompt", conversation_get_prompt_overrider) if args.no_context else contextlib.nullcontext():
-        with mock.patch.object(Conversation, "append_message", conversation_append_message) if args.no_context else contextlib.nullcontext():
+
+    with mock.patch.object(
+        Conversation, "get_prompt", conversation_get_prompt_overrider
+    ) if args.no_context else contextlib.nullcontext():
+        with mock.patch.object(
+            Conversation, "append_message", conversation_append_message
+        ) if args.no_context else contextlib.nullcontext():
             cli.main(args)
 
 
---
+++ jallm/modeling_utils.py
@@ -0,0 +1,55 @@
+import os
+from typing import Optional, Tuple, Union
+
+import torch
+from fastchat.model.model_adapter import load_model
+from peft import LoraConfig, set_peft_model_state_dict, get_peft_model, PeftModel
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
+
+
+def load_lora_model(
+    model_path: str,
+    lora_weight: str,
+    device: str,
+    num_gpus: int,
+    max_gpu_memory: Optional[str] = None,
+    load_8bit: bool = False,
+    cpu_offloading: bool = False,
+    debug: bool = False,
+) -> Tuple[Union[PreTrainedModel, PeftModel], PreTrainedTokenizerBase]:
+    model: Union[PreTrainedModel, PeftModel]
+    tokenizer: PreTrainedTokenizerBase
+    model, tokenizer = load_model(
+        model_path=model_path,
+        device=device,
+        num_gpus=num_gpus,
+        max_gpu_memory=max_gpu_memory,
+        load_8bit=load_8bit,
+        cpu_offloading=cpu_offloading,
+        debug=debug,
+    )
+    if lora_weight is not None:
+        # model = PeftModelForCausalLM.from_pretrained(model, model_path, **kwargs)
+        config = LoraConfig.from_pretrained(lora_weight)
+        model = get_peft_model(model, config)
+
+        # Check the available weights and load them
+        checkpoint_name = os.path.join(
+            lora_weight, "pytorch_model.bin"
+        )  # Full checkpoint
+        if not os.path.exists(checkpoint_name):
+            checkpoint_name = os.path.join(
+                lora_weight, "adapter_model.bin"
+            )  # only LoRA model - LoRA config above has to fit
+        # The two files above have a different name depending on how they were saved,
+        # but are actually the same.
+        if os.path.exists(checkpoint_name):
+            adapters_weights = torch.load(checkpoint_name)
+            set_peft_model_state_dict(model, adapters_weights)
+        else:
+            print(f"Checkpoint {checkpoint_name} not found")
+
+    if debug:
+        print(model)
+
+    return model, tokenizer
